b1fb643f230d8ade33c4a98b594e88af3969dcb7
VitorNeves <joao.vitor.f.naves@accenture.com>
2019-10-22 20:31:49 -0300

    Add project test

---
+++ nao e um jogo/Assets/Scripts/MoveOffSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffSet : MonoBehaviour
+{
+    private Material currentMaterial;
+    public float speed;
+    private float offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentMaterial = GetComponent<Renderer>().material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        offSet += 0.001f;
+        currentMaterial.SetTextureOffset ("Sprites_default", new Vector2 (offSet*speed,0));
+    }
+}
